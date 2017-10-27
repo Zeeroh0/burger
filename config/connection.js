c46e584b7b8bf1ecc09cf13e56783d31bedbe4d1
jtmPC <jon87tyler@gmail.com>
2017-10-27 00:00:22 -0700

    updated connection.js to be ready for Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,35 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "burgers_db"
+// });
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
 
-  console.log(`connected to the ${connection.config.database} database as id ${connection.threadId}`);
-});
+//   console.log(`connected to the ${connection.config.database} database as id ${connection.threadId}`);
+// });
+
+// To use the DB on Heroku:
+var connection;
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		host: "localhost",
+	  user: "root",
+	  password: "",
+	  database: "burgers_db"
+	});
+}
+
+connection.connect();
 
 module.exports = connection;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
 
 const app = express();
-const PORT = process.env.PORT || 1400;
+const PORT = process.env.PORT || 8080;
 
 // Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
